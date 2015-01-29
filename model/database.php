eaad75b4ef12e7635e6e9fba50af6e4e95289890
Jorge Hernandez <jorgeh905187@gmail.com>
2015-01-29 11:22:19 -0800

    ran and fixed the php file
    ran and fixed the php file on my blog website

--- model/database.php
+++ model/database.php
@@ -1,8 +1,7 @@
 <?php
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "blog_db";
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
