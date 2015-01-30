7793160e9a4954daf11c851a923156933355d110
Jorge Hernandez <jorgeh905187@gmail.com>
2015-01-30 11:27:52 -0800

    created a database
    created a database for my blog website

--- controller/create-db.php
+++ controller/create-db.php
@@ -6,16 +6,25 @@
     if($connection->connect_error) {
         die("Error: " . $connection->connect_error);
     }
-    else {
-        echo "Success: " . $connection->host_info;
-    }
     
     $exists = $connection->select_db($database);
     
     if(!$exists) {
-        echo "Database does not exist";
+        $query = $connection->query("CREATE DATABASE $database");
+        
+        if($query) {
+            echo "Successfully created database: " . $database;
+        }
+    }
+    else {
+        echo "Database already exists.";
     }
     
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY (id))");
     
     $connection->close();
     
